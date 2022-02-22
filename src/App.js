1e611ddb16992d5409a2672dd75ee59a24360458
Prabesh <prabesh.bista369@gmail.com>
2022-02-22 10:28:57 +0545

    .

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import AddUser from "./components/Users/AddUsers";
+import UserList from "./components/Users/Users_list";
 
 function App() {
   return (
     <div className="App">
     <AddUser/>
-      
+    <UserList users={[{"name":"Prabesh","age":20}]} />  
     </div>
   );
 }
--- src/components/Users/Users_list.js
+++ src/components/Users/Users_list.js
@@ -1,16 +1,20 @@
 import React from "react";
 import Card from "../UI/card";
-import classes from "UserList.module.css";
+import classes from "./UserList.module.css";
 
 const UserList=(props)=>{
     return(
         <Card className={classes.users}>
             <ul>
-                {
-                    props.users.map((user)=><li>{user.name}
-                     ({user.age} years old)</li>)
+                {props.users.map((user)=>(
+                    <li>
+                    {user.name}({user.age} years old)
+                     </li>))
+                    
                 }
             </ul>
         </Card>
     );
 }
+
+export default UserList;
