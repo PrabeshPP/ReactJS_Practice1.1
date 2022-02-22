b95a9073454b973d0a4da729ecc6d78d30b4a7d3
Prabesh <prabesh.bista369@gmail.com>
2022-02-22 09:29:39 +0545

    resetting the input value

--- src/components/Users/AddUsers.js
+++ src/components/Users/AddUsers.js
@@ -4,12 +4,14 @@ import classes from './AddUsers.module.css';
 import Button from "../UI/Button";
 
 function AddUser(props){
-    const [enteredUsername,setEnteredUsername]=useState('');
-    const [enteredAge,setEnteredAge]=useState('');
+    const [enteredUsername,setEnteredUsername]=useState("");
+    const [enteredAge,setEnteredAge]=useState("");
 
     const addUserHandler=(event)=>{
         event.preventDefault();
         console.log(enteredUsername,enteredAge);
+        setEnteredUsername('');
+        setEnteredAge('');
     };
 
     const userNameChangeHandler=(event)=>{
@@ -23,11 +25,13 @@ function AddUser(props){
 
     return(
         <Card classes={classes.input}>
-        <form onSubmit={addUserHandler}>
+        <form onSubmit={
+            addUserHandler
+        }>
             <label htmlFor="username">Username</label>
-          <input  id="username" type={"text"} onChange={userNameChangeHandler} />
+          <input  id="username" type={"text"} value={enteredUsername} onChange={userNameChangeHandler} />
           <label htmlFor="age">Age</label>
-          <input id="age" type={"number"} onChange={userAgeChangeHandler} />
+          <input id="age" type={"number"} value={enteredAge} onChange={userAgeChangeHandler} />
 
           <Button type="submit">Add User</Button>
         </form>
