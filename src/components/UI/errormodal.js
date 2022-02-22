09a108bdfafd0481e60b17f3d2014d585ac35e5f
Prabesh <prabesh.bista369@gmail.com>
2022-02-22 13:59:50 +0545

    .

--- src/components/UI/errormodal.js
+++ src/components/UI/errormodal.js
@@ -1,6 +1,27 @@
 import React from "react";
+import Button from "./Button";
+import Card from "./card";
 
 
 const  ErrorModal=(props)=>{
-    
+    return(
+        <Card>
+        <header>
+            <h2>{props.title}</h2>
+        </header>
+        <div>
+            <p>{props.message}</p>
+        </div>
+
+        <footer>
+            <Button>
+                Okay
+            </Button>
+        </footer>
+
+        </Card>
+
+    );
 }
+
+export default ErrorModal;
