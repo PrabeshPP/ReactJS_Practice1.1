5f57d24a7c7c21d26a8eff3c9a3a5cf5920c7f5b
Prabesh <prabesh.bista369@gmail.com>
2022-02-24 19:04:39 +0545

    Using React Portal

--- src/components/UI/errormodal.js
+++ src/components/UI/errormodal.js
@@ -1,16 +1,19 @@
 import React from "react";
 import Button from "./Button";
 import Card from "./card";
+import ReactDom from "react-dom";
 
 import classes from "./errormodal.module.css";
 
-
-const  ErrorModal=(props)=>{
-    return(
-        //! Using the React.Fragment
-        <React.Fragment>
+const Backdrop=(props)=>{
+    return (
         <div className={classes.backdrop} onClick={props.onConfirm}></div>
-            <Card classes={classes.modal}>
+    );
+}
+
+const BackDropOverlay=(props)=>{
+    return(
+        <Card classes={classes.modal}>
         <header className={classes.header}>
             <h2>{props.title}</h2>
         </header>
@@ -23,8 +26,29 @@ const  ErrorModal=(props)=>{
                 Okay
             </Button>
         </footer>
-
         </Card>
+    );
+}
+
+
+const  ErrorModal=(props)=>{
+    return(
+        //! Using the React.Fragment
+        <React.Fragment>
+        {
+            ReactDom.createPortal(
+                <Backdrop onConfirm={props.onConfirm}/>,
+                document.getElementById('backdrop--root')
+            )
+        }
+
+        ReactDom.createPortal(
+            <BackDropOverlay title={props.title} message={props.message} onConfirm={props.onConfirm}/>
+
+        )
+           
+            
+           
         </React.Fragment>
         
 
